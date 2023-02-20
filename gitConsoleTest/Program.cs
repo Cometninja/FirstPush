791f7ce24f938d4fdaecf9ae0b5e5bb7a0e64795
Daniel Casey <daniel_mn_casey@hotmail.com>
2023-02-20 21:44:43 +0000

    i have made another update

--- gitConsoleTest/Program.cs
+++ gitConsoleTest/Program.cs
@@ -5,7 +5,7 @@ namespace gitConsoleTest{
     class Program{
 
         static void Main(string[] args){
-            Console.WriteLine("hello daniel i am the new git thingy");
+            Console.WriteLine("i have now added in all the files for the code so far");
         }
     }
 }
